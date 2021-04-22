ee0a8c44e7142a8adc3e06ed35466fbe36922743
unknown <ddaksh.2711@gmail.com>
2021-04-22 18:00:51 +0530

    add user schema

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const passport = require("passport");
+
 const app = express();
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());   
-
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 const dbsURI = 'mongodb+srv://Enkrypzo:enkrypzo@encclus.m4q6d.mongodb.net/Enkrypzo?retryWrites=true&w=majority';
 mongoose.connect(dbsURI,{useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set("useCreateIndex", true);
 
-app.use(express.static("public"));
-app.set("view engine", "ejs");
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -21,6 +26,13 @@ app.get("/", function (req, res) {
 
 app.get("/login", function (req, res) {
   res.render("login");
+});
+
+app.post("/login", (req,res) => {
+  const user = new User({
+    username: req.body.email,
+    password: req.body.password,
+  });
 });
 
 app.get("/register", function (req, res) {
