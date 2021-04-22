6e915b57bf3b93bfb68847e1dde102582b8b0c36
shivam8812 <shivag4321@gmail.com>
2021-04-22 17:30:33 +0530

    connect with mongodb

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const app = express();
-
+const dbsURI = 'mongodb+srv://Enkrypzo:enkrypzo@encclus.m4q6d.mongodb.net/Enkrypzo?retryWrites=true&w=majority';
+mongoose.connect(dbsURI,{useNewUrlParser: true, useUnifiedTopology: true});
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
